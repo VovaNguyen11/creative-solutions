09a9b6c9437573cda2ecbffc47019de1852b5170
VovaNguyen11 <vovanguyen11@gmail.com>
2019-03-11 16:59:24 +0200

    updated styles for mobile version

--- src/js/common.js
+++ src/js/common.js
@@ -36,7 +36,7 @@
             slidesToShow: 1,
             slidesToScroll: 1,
             dots: true,
-            autoplay: true,
+            autoplay: false,
             autoplaySpeed: 4000,
             infinite: true,
             responsive: [
@@ -72,5 +72,41 @@
                 }
             ]
         });
+
+        // Select all links with hashes
+        $('a[href*="#"]')
+            // Remove links that don't actually link to anything
+            .not('[href="#"]')
+            .not('[href="#0"]')
+            .click(function (e) {
+                // On-page links
+                if (
+                    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                    location.hostname == this.hostname
+                ) {
+                    // Figure out element to scroll to
+                    var target = $(this.hash);
+                    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                    // Does a scroll target exist?
+                    if (target.length) {
+                        // Only prevent default if animation is actually gonna happen
+                        e.preventDefault();
+                        $('html, body').animate({
+                            scrollTop: target.offset().top
+                        }, 1000, function () {
+                            // Callback after animation
+                            // Must change focus!
+                            var $target = $(target);
+                            $target.focus();
+                            if ($target.is(":focus")) { // Checking if the target was focused
+                                return false;
+                            } else {
+                                $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                                $target.focus(); // Set focus again
+                            };
+                        });
+                    }
+                }
+            });
     });
 })(jQuery)
