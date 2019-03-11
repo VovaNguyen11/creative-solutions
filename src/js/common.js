e9e507e945d0aa01bd92babc1960644287611b26
VovaNguyen11 <vovanguyen11@gmail.com>
2019-03-11 14:31:26 +0200

    made adaptive for mobiles and tablets

--- src/js/common.js
+++ src/js/common.js
@@ -12,13 +12,25 @@
                 $header.removeClass('sticky');
                 scrollPosition = window.scrollY - 1;
                 console.log('w' + window.scrollY);
-                if(window.scrollY > 200){
+                if (window.scrollY > 100) {
                     $header.removeClass('header--transparent');
-                }  else{
+                } else {
                     $header.addClass('header--transparent');
                 }
             }
         });
+
+        //Adaptive Menu
+        $(".menu-icon").click(() => {
+              $(".nav__list").toggleClass("showing");
+        });
+
+
+        //Back To Top
+        const $backToTop = $('.back-to-top');
+        $backToTop.click(() => {
+            $('html').animate({ scrollTop: 0 }, 1000); 
+        })
 
         $('.works-slider').slick({
             slidesToShow: 1,
@@ -26,38 +38,39 @@
             dots: true,
             autoplay: true,
             autoplaySpeed: 4000,
-            infinite: true
-            // responsive: [{
-            //         breakpoint: 1400,
-            //         settings: {
-            //             slidesToShow: 1,
-            //             arrows: false,
-            //             draggable: false,
-            //         }
-            //     },
-            //     {
-            //         breakpoint: 1024,
-            //         settings: {
-            //             slidesToShow: 1,
-            //             arrows: false,
-            //             draggable: true,
-            //         }
-            //     },
-            //     {
-            //         breakpoint: 600,
-            //         settings: {
-            //             slidesToShow: 1,
-            //             arrows: false,
-            //             draggable: true
-            //         }
-            //     }
-            // ]
+            infinite: true,
+            responsive: [
+                {
+                    breakpoint: 769,
+                    settings: {
+                        arrows: false,
+                        draggable: true
+                    }
+                }
+            ]
         });
+
         $('.team-slider').slick({
             slidesToShow: 3,
             slidesToScroll: 1,
             dots: false,
-            infinite: false
+            infinite: false,
+            arrows: true,
+            responsive: [
+                {
+                    breakpoint: 1025,
+                    settings: {
+                        slidesToShow: 2
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1,
+                        arrows: false
+                    }
+                }
+            ]
         });
     });
 })(jQuery)
